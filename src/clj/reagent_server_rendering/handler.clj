5c45eba423573ef0f7a26d43e56d14e0f5419b2f
ThinhP.Tran <trphthinh@gmail.com>
2016-11-27 17:25:01 +0700

    Add a Clojure namespace for the server functions

--- src/clj/reagent_server_rendering/handler.clj
+++ src/clj/reagent_server_rendering/handler.clj
@@ -1,1 +1,13 @@
-(ns reagent-server-rendering.handler)
+(ns reagent-server-rendering.handler
+  (:require [aleph.flow :as flow]
+            [clojure.java.io :as io]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [include-js include-css]]
+            [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [not-found resources]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]])
+  (:import [io.aleph.dirigiste Pools]
+           [javax.script ScriptEngineManager Invocable]))
+
+
+
